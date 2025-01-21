6fdf86300404a3f712e1686f59ac0b760f04c9be
OminousShadow87 <brucewaynephil@gmail.com>
2025-01-21 10:59:48 -0800

    Added Point and Line Classes

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from window_class import *
+from window_line_point import *
 
 def main():
     win = Window(800, 600)
+    #testing info below - no need to keep
+    point1 = Point(50, 50)
+    point2 = Point(200, 200)
+    line1 = Line(point1, point2)
+    win.draw_line(line1, "black")
+    point3 = Point(69, 420)
+    line2 = Line(point2, point3)
+    win.draw_line(line2, "red")
+    #testing info above - no need to keep
     win.wait_for_close()
 
 if __name__ == "__main__":
--- window_line_point.py
+++ window_line_point.py
@@ -22,3 +22,20 @@ class Window:
     
     def close(self):
         self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y    
+
+class Line:
+    def __init__(self, point_a, point_b):
+        self.a = point_a
+        self.b = point_b
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.a.x, self.a.y, self.b.x, self.b.y, fill=fill_color, width=2)
+         
